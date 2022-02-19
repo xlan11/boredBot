5c81550193ef2901adbadd55838cde57983ea66b
Alan Kelly <alankelly@gmail.com>
2022-02-19 22:10:00 +0000

    succesfully getting data from bored.api

---
+++ index.js
@@ -0,0 +1,8 @@
+document.getElementById("idea-btn").addEventListener("click", function(){
+    fetch ("https://apis.scrimba.com/bored/api/activity")
+        .then (response => response.json())
+        .then (data => {
+            document.getElementById("new-idea").textContent = data.activity
+        }
+        )
+})
